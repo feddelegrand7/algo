ab54b663c2cfaead6dc6bc4111be800de66208bc
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-06-16 23:24:53 +0200

    use_algolia documented

--- R/use_algolia.R
+++ R/use_algolia.R
@@ -1,4 +1,22 @@
 
+#' Enable the Algolia Places JavaScript library
+#'
+#' @description The function activates the capabilities of the Algolia Places JavaScript library
+#'
+#' @return called for the side effect of activating the Algolia Places JavaScript library
+#' @export
+#'
+#' @examples \donttest{
+#'
+#' # Set the algolia function at the beginning of the shiny app UI
+#'
+#' use_algolia()
+#'
+#'
+#'
+#' }
+
+
 use_algolia <- function(){
 
   htmltools::tags$script(src="https://cdn.jsdelivr.net/npm/places.js@1.19.0")
