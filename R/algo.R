eca2e0460e05ca4fb7c393ac8bf5a81a78ef6f5d
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-06-16 22:09:46 +0200

    Initial commit

---
+++ R/algo.R
@@ -0,0 +1,37 @@
+algo <- function(element){
+
+  ID <- Sys.getenv('ALGOLIA_ID')
+  KEY <- Sys.getenv('ALGOLIA_KEY')
+
+  if (identical(ID, "") || identical(KEY, "")) {
+    stop("Please set the Algolia ID and KEY within your R environment: name them ALGOLIA_ID and ALGOLIA_KEY, respectively",
+         call. = FALSE)
+  }
+
+
+
+
+  htmltools::tags$script(HTML(
+
+  glue::glue(
+
+    "
+
+  var placesAutocomplete = places({{
+      appId: '{ID}',
+      apiKey: '{KEY}',
+      container: document.querySelector('{element}')
+    }});
+
+
+  "
+
+
+  )
+
+  ))
+
+
+
+
+}
---
+++ R/use_algolia.R
@@ -0,0 +1,7 @@
+
+use_algolia <- function(){
+
+  htmltools::tags$script(src="https://cdn.jsdelivr.net/npm/places.js@1.19.0")
+
+
+  }
